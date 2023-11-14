41042936b9b3ea66e85d606805b02c5917888731
Yared <yared241993@gmail.com>
2023-11-14 17:31:01 +0300

    sth updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Display from "./components/Display";
+import HeaderApp from "./components/HeaderApp";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div>
       <div className="text-center ">
-        {" "}
+        <HeaderApp />
+        <Message />
         <Display />
       </div>
       <Counter />
--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const HeaderApp = () => {
   return (
     <div>
-      <div className="bg-black p-3">
-        <h className="text-3xl text-white">Redux Toolkit (RTK)</h>
+      <div className="bg-gray-200 p-3">
+        <h className="text-3xl text-gray-500">Redux Toolkit (RTK)</h>
       </div>
     </div>
   );
