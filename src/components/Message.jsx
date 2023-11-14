f29e6656080a7b6deb42f3b118a9a1edaa1ccf4f
Yared <yared241993@gmail.com>
2023-11-14 17:30:13 +0300

    sth updated

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Message = () => {
-  return <div></div>;
+  return (
+    <div className="space-y-4">
+      <h1>Redux (RTK) State Management System</h1>
+    </div>
+  );
 };
 
 export default Message;
