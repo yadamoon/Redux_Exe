2a472abd0632aa10527a451662a0e63c88bc87d9
Yared <yared241993@gmail.com>
2023-11-14 17:07:53 +0300

    create slice

---
+++ src/Features/counter/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateValues = {
+  coin: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialStateValues,
+  reducers: {
+    increment: (state) => {
+      state.coin += 1;
+    },
+    decrement: (state) => {
+      state.coin -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.coin += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.coin -= action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount, decrementByAmount } =
+  counterSlice.actions;
+
+export default counterSlice.reducer;
