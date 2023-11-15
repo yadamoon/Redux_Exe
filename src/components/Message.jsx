bb68c5454df84b53707c05df5660174a261bac56
Yared <yared241993@gmail.com>
2023-11-15 14:41:54 +0300

    updated sth

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -7,6 +7,7 @@ const Message = () => {
       <p>
         Redux Toolkit is an official opinionated set of utilities and tools that
         simplifies the process of working with <i> Redux</i>
+        <p>and redux are a state management system</p>
       </p>
     </div>
   );
